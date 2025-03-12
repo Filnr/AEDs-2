7e9589785ed0aba9c17a5c1eb374a807df2f30af
Filipe <fineryrabelo@gmail.com>
2025-03-12 14:06:21 -0300

    subString

--- Tps/tp01/ex11/Substring.java
+++ Tps/tp01/ex11/Substring.java
@@ -1,59 +1,68 @@
-//package ex11;
 import java.util.*;
+
 public class Substring {
+    
     public static boolean ehFim(String entrada) {
-        	//Função que verifica se a entrada é o FIM
-        	//Inicialmente apenas compara se a entrada tem o tamanho certo para ser FIM depois verificaa string
-        	boolean fim = false;
-        	if (entrada.length() == 3) {
-            	if ((entrada.charAt(0) == 'F') && (entrada.charAt(1) == 'I') && (entrada.charAt(2) == 'M')) {
+        //Função que verifica se a entrada é o FIM
+        //Inicialmente apenas compara se a entrada tem o tamanho certo para ser FIM depois verificaa string
+        boolean fim = false;
+        if (entrada.length() == 3) {
+            if ((entrada.charAt(0) == 'F') && (entrada.charAt(1) == 'I') && (entrada.charAt(2) == 'M')) {
                 fim = true;
-            	}
-        	}
-        	return fim;
-	}
-
-    public static int encontraSub(String entrada){
-        // Função que busca a quantidade de letras da substring
-        int qt = 0;
-        // Variavel que controla se um numero é repetido ou não
-        int qtIguais = 0;
-        // Array que salva as letras ja lidas que não são repetidas
-        char[] letras = new char[entrada.length()];
-        for(int i = 0; i < entrada.length(); i++){
-            int j = 0;
-            while(j < i){
-                // se uma letra igual for encontrada aumenta o qtIguais
-                if(letras[j] == entrada.charAt(i)){
-                    qtIguais++;
-                    j = i;
-                }
-                else{
-                    j++;
-
-                }
-            }
-            // se não houver letras iguais registrada, registra a letra, e aumenta em 1 o qt
-            if(qtIguais == 0){
-                letras[i] = entrada.charAt(i);
-                qt++;
-            }
-            // se houver letras repetidas, encerra o loop
-            else if(qtIguais > 0){
-                
             }
         }
-        return qt;
+        return fim;
     }
 
-    public static void main(String[] args){
-        // Troca a entrada padrão para ler corretamente
+    public static void limpaArray(char[] letras) {
+        for (int i = 0; i < letras.length; i++) {
+            letras[i] = '\0';
+        }
+    }
+
+    public static int encontraSub(String entrada) {
+        int qt = 0;
+        int maior = 0;
+        char[] letras = new char[entrada.length()];
+        int inicio = 0; // Índice do início da substring sem repetição
+        for (int i = 0; i < entrada.length(); i++) {
+            char atual = entrada.charAt(i);
+            boolean repetido = false;
+            int j = inicio;
+            // Verifica se o caractere já existe na substring atual
+            while (j < qt) {
+                if (letras[j] == atual) {
+                    repetido = true;
+                    // Atualiza o índice de início para o próximo da repetição
+                    inicio = j + 1;
+                }
+                j++;
+            }
+            if (repetido) {
+                // Atualiza o maior tamanho antes de reiniciar a contagem
+                if (qt - inicio > maior) {
+                    maior = qt - inicio;
+                }
+            }
+            // Adiciona o caractere à lista de caracteres únicos da substring
+            letras[qt] = atual;
+            qt++;
+            // Atualiza o maior tamanho encontrado
+            if (qt - inicio > maior) {
+                maior = qt - inicio;
+            }
+        }
+        return maior;
+    }
+
+    public static void main(String[] args) {
+        // Função responsavel por comandar o funcionamento do programa
         Scanner ler = new Scanner(System.in, "UTF-8");
         String entrada = ler.nextLine();
-        while(!ehFim(entrada)){
-            // Ja chama a função que que retornaa resposta no println
+        while (!ehFim(entrada)) {
             System.out.println(encontraSub(entrada));
             entrada = ler.nextLine();
         }
+        ler.close();
     }
 }
