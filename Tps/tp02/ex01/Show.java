7d3a6942e01a9902a318f12ae2ae7650e4b39206
Filipe <fineryrabelo@gmail.com>
2025-03-31 15:03:18 -0300

    TP2

---
+++ Tps/tp02/ex01/Show.java
@@ -0,0 +1,25 @@
+import java.util.*;
+//simple date format e date
+public class Show {
+    private
+        String show_ID;
+        String type;
+        String title;
+        String director;
+        String[] cast;
+        String country;
+        Date data_added;
+        int release_year;
+        String rating;
+        String duration;
+        String[] listed_in; 
+
+    public Show(){
+        show_ID = "";
+        type = "";
+        title = "";
+        director = "";
+        country = "";
+        
+    }
+}
